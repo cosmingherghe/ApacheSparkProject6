85eb37a3c53323916ca2b8919cc35a738794d1f5
Cosmin Gherghe <cosminlinux@gmail.com>
2020-11-07 01:18:42 +0000

    Adding README.md & Aggregate functions to finish the project

---
+++ README.md
@@ -0,0 +1,3 @@
+### Working with real world data using Reddit file comments to simulate a spark job that can run on Amazon using the AWOS services
+- When you're working with distributed data, the source of the data the file 
+should reside on a distributed file system.  Instead of the local file we can use Amazon S3 bucket.
--- src/main/java/dev/cosmingherghe/spark/Application.java
+++ src/main/java/dev/cosmingherghe/spark/Application.java
@@ -6,6 +6,7 @@ import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.*;
 
 import java.util.Arrays;
+import static org.apache.spark.sql.functions.*;
 
 public class Application {
 
@@ -20,7 +21,7 @@ public class Application {
                 .master("local")
                 .getOrCreate();
 
-        String redditFile = "D:\\_IntelliJ-worksspace\\ApacheSpark\\Files\\RC_2007-01-SmallFile";
+        String redditFile = "D:\\_IntelliJ-worksspace\\ApacheSpark\\Files\\RC_2011-02-LargeFile";
 
         Dataset<Row> redditDf = spark.read().format("json")
                 .option("inferSchema", true) // Make sure to use string version of true
@@ -30,11 +31,24 @@ public class Application {
         //find the most commonly used word by doing a count of occurrences
         redditDf = redditDf.select("body");
         Dataset<String> wordsDs = redditDf.flatMap( (FlatMapFunction<Row, String>)
-                        r -> Arrays.asList( r.toString().replace("\n", "").trim().toLowerCase()
+                        r -> Arrays.asList( r.toString().replace("\n", "").replace("\r", "").trim().toLowerCase()
                         .split(" ")).iterator(),
                 Encoders.STRING());
 
         Dataset<Row> wordsDf = wordsDs.toDF();
+
+        //create a data set and exclude boring words
+        Dataset<Row> boringWordsDf = spark.createDataset(Arrays.asList(WordUtils.stopWords), Encoders.STRING()).toDF();
+        //wordsDf = wordsDf.except(boringWordsDf); // <-- this removes duplicates
+
+        //join type "felttanti"  gives us a subtraction but without removing the duplicates
+        wordsDf = wordsDf.join(boringWordsDf, wordsDf.col("value").equalTo(boringWordsDf.col("value")), "leftanti");
+
+        //the aggregate function - is going to return a data frame called wordsDf with another column.
+        wordsDf = wordsDf.groupBy("value").count();
+
+        //display the count of each word in a descending fashion.
+        wordsDf.orderBy(desc("count")).show();
 
     }
 }
